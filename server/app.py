65af0f47abf352ecdece5abaf5a719cb36b4afad
Robert <rlgallegos85@gmail.com>
2023-10-01 00:43:24 -0400

    initial route setup, checking ports

---
+++ server/app.py
@@ -0,0 +1,19 @@
+from flask import request
+from flask_restful import Api, Resource
+
+from config.py import app
+
+api = Api(app)
+
+class User(Resource):
+    def post(self):
+        data = request.get_json()
+        print(data)
+
+
+api.add_resource(User, '/user')
+
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
