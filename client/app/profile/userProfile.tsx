131ccfeead52edef6ea1ab70dc0857ba037a1ff7
Robert <rlgallegos85@gmail.com>
2023-10-07 01:42:22 -0400

    completed front end update image fetch

--- client/app/profile/userProfile.tsx
+++ client/app/profile/userProfile.tsx
@@ -1,24 +1,42 @@
 'use client'
-import Image from "next/image"
-
 import { User, Restaurant } from "@/interfaces"
 
 import AddRestaurantForm from "./addRestaurantForm"
+import ClickableImage from "../components/clickableImage"
+import { useState } from "react"
 
 interface Props{
     userData: User,
     restaurantData: Restaurant[]
 }
 
-
 export default function UserProfile({userData, restaurantData}: Props){
+    const [error, setError] = useState<string>('')
 
+    async function handleUpdateImage(file: File){
+        if (file){
+            const request = new FormData()
+            request.append('file', file as Blob, 'image.jpg')
 
-    // function handleUpdateImage(e: )
+            const res = await fetch(`${process.env.NEXT_PUBLIC_REACT_APP_API}/user/${userData.id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: request
+            })
+            if (res.ok){
+                res.json().then(data => console.log(data))
+            } else {
+                res.json().then(e => setError(e.error))
+            }
+        }
+    }
 
     return <div>
         <div className='w-full mx-auto flex flex-col items-center justify-center'>
-            <Image src={userData.image} alt='user' height={300} width={300} />
+            <ClickableImage src={userData.image} alt='user' height={300} width={300} callback={handleUpdateImage} />
+            {error && <p>{error}</p>}
             <h1>{userData.name}</h1>
             <h3>Email: {userData.email}</h3>
             <h3>Restaurant: {userData.restaurant ? userData.restaurant.name : 'None Selected'}</h3>
